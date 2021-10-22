8b3283829891c8f29ec98e421328733abd8325a5
lucas barra <lucastbarra@hotmail.com>
2021-10-21 23:46:55 -0300

    filayout home

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,20 +1,46 @@
 import styles from './styles.module.scss';
 
 import logoImg from '../../assets/logo.svg';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
 
 export function MessageList() {
+    const [messages, setMessages] = useState<Message[]>();
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessages(response.data);
+        })
+    }, [])
+
     return (
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="logo" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem doloribus ab, id incidunt distinctio, alias cupiditate illo consequuntur iusto perspiciatis
-                        sed vel odio ad accusantium doloremque molestias, culpa sint dolore.
-                    </p>
-                    <div className={styles.messageUser}></div>
-                </li>
+                {messages?.map(message => (
+                    <li key={message.id} className={styles.message}>
+                        <p className={styles.messageContent}>
+                        {message.text}
+                        </p>
+                        <div className={styles.messageUser}>
+                            <div className={styles.userImage}>
+                                <img src={message.user.avatar_url} alt={message.user.name} />
+                            </div>
+                            <span>{message.user.name}</span>
+                        </div>
+                    </li>
+                ))}
+
             </ul>
         </div>
     )
